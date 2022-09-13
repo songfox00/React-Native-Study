14a1088abbad50c58cec5219052d3ca9b8195293
songfox00 <songfox00@gmail.com>
2022-09-13 13:38:10 +0900

    reset

--- android/app/src/main/kotlin/com/example/smsw/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.smsw
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/color.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-final greenColor=0xffFF5D5D;
