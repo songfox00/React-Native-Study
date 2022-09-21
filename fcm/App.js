0511d85628f02a716cabbf64647d567273c74f0d
songfox00 <songfox00@gmail.com>
2022-09-22 08:33:10 +0900

    fcm 구현

--- fcm/App.js
+++ fcm/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import {Alert, Text} from 'react-native';
 import messaging from '@react-native-firebase/messaging';
+import DeviceInfo from 'react-native-device-info';
 
 export default App = () => {
   useEffect(() => {
@@ -8,6 +9,12 @@ export default App = () => {
       // await firebase.initializeApp(credentials, config);
       const token = await messaging().getToken();
       if (token) console.log(token);
+
+      DeviceInfo.getDeviceName().then(deviceName => {
+        console.log('deviceName: ', deviceName);
+      });
+      console.log('model: ', DeviceInfo.getModel());
+      console.log('DeviceId:', DeviceInfo.getDeviceId());
     };
     checkToken();
   });
