704b5a7bf9f863a6e42d304129b4509e88011dd4
songfox00 <songfox00@gmail.com>
2022-11-01 11:13:49 +0900

    key 숨기기

--- collection/ios/collection/AppDelegate.mm
+++ collection/ios/collection/AppDelegate.mm
@@ -40,7 +40,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"AIzaSyCJICif02_NPiB34DdexPZsFkv0ZIhl4Ok"];
+  [GMSServices provideAPIKey:@"${Config.IOS_GOOGLEKEY}"];
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
